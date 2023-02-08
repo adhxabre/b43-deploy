82ad47ef1c1a0a9f3a95f47e11d8271a637feeb2
adhxabre <abeldustin06@gmail.com>
2023-02-08 09:03:00 +0700

    Deploy

--- server/pkg/mysql/mysql.go
+++ server/pkg/mysql/mysql.go
@@ -2,8 +2,10 @@ package mysql
 
 import (
 	"fmt"
+	"os"
 
 	// Import postgres package here ...
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -13,6 +15,11 @@ func DatabaseInit() {
 	var err error
 
 	// Get `host`, `user`, `password`, `database name`, and `port` from env here ...
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 
 	// ===== If using MySql =====
 	// dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
@@ -20,6 +27,8 @@ func DatabaseInit() {
 
 	// ===== If using Postgres =====
 	// Setup database connection here ...
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
